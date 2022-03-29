6cad5b801e2fdb1bd6cfee76bfa28e92164d43cf
sarthak-nik <sarthaksham@gmail.com>
2022-03-29 12:09:37 +0530

    db updated for getting data

--- app/src/main/java/com/anusarwant/ctms/DBHandler.java
+++ app/src/main/java/com/anusarwant/ctms/DBHandler.java
@@ -2,6 +2,7 @@ package com.anusarwant.ctms;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -17,7 +18,7 @@ public class DBHandler extends SQLiteOpenHelper {
     //creating variable for tournament name column
     private static final String TOURNAMENT_COL = "tournament";
     //creating variable for match number column
-    private static final String MATCH_COL = "match";
+    private static final String MATCH_COL = "matches";
     //creating variable for innings number column
     private static final String INNINGS_COL = "innings";
     //creating variable for player name column
@@ -26,6 +27,10 @@ public class DBHandler extends SQLiteOpenHelper {
     private static final String RUNS_COL = "runs_scored";
     //creating variable for balls played column
     private static final String BALLS_COL = "ballsPlayed";
+    //creating variable for fours name column
+    private static final String FOUR_COL = "fours";
+    //creating variable for six name column
+    private static final String SIX_COL = "sixes";
     //creating variable for strike rate column
     private static final String STRIKE_COL = "strikeRate";
     //creating variable for batting average column
@@ -51,11 +56,12 @@ public class DBHandler extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         String query = "CREATE TABLE " + TABLE_NAME + " ("
                 + TOURNAMENT_COL + " TEXT, "
-                + MATCH_COL + "INTEGER,"
-                + INNINGS_COL + " INTEGER,"
-                + PLAYER_COL + " INTEGER,"
+                + MATCH_COL + "TEXT,"
+                + PLAYER_COL + " TEXT,"
                 + RUNS_COL + "INTEGER,"
                 + BALLS_COL + "INTEGER,"
+                + FOUR_COL + "INTEGER,"
+                + SIX_COL + "INTEGER,"
                 + STRIKE_COL + "REAL,"
                 + BATAVG_COL + "REAL,"
                 + WICKETS_COL + "INTEGER,"
@@ -67,7 +73,7 @@ public class DBHandler extends SQLiteOpenHelper {
 
         db.execSQL(query);
     }
-    public void addNewRow(String tournament,int match,int innings,int player,int runs,int ballsPlayed,
+    public void addNewRow(String tournament,int match,String player,int runs,int ballsPlayed,int fours,int sixes,
                           float strikeRate,float batAvg,int ballsBowled,int wickets,int runsGiven,float economy,float bowlAvg)
     {
 
@@ -80,23 +86,39 @@ public class DBHandler extends SQLiteOpenHelper {
         //storing all values
         values.put(TOURNAMENT_COL,tournament);
         values.put(MATCH_COL,match);
-        values.put(INNINGS_COL,innings);
         values.put(PLAYER_COL,player);
         values.put(RUNS_COL,runs);
         values.put(BALLS_COL,ballsPlayed);
+        values.put(FOUR_COL,fours);
+        values.put(SIX_COL,sixes);
         values.put(STRIKE_COL,strikeRate);
         values.put(BATAVG_COL,batAvg);
-        values.put(BALLSBOWLED_COL,ballsBowled);
         values.put(WICKETS_COL,wickets);
+        values.put(BALLSBOWLED_COL,ballsBowled);
+        values.put(RUNSGIVEN_COL,runsGiven);
         values.put(ECONOMY_COL,economy);
         values.put(BOWLAVG_COL,bowlAvg);
 
         //adding content values to the table
         db.insert(TABLE_NAME, null, values);
-
         // at last we are closing our database
         db.close();
     }
+
+    public Cursor getDetails(String tournament,int matchNum,String player,int runs,int ballsPlayed,int fours,int sixes,double strikeRate,
+                             double batAvg,int wickets,int ballsBowled,int runsGiven,double economy,double bowlAvg){
+        //getting our readable database
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        //creating a cursor
+        Cursor cursor=db.rawQuery("SELECT * FROM "+TABLE_NAME+" WHERE "+TOURNAMENT_COL+"=? AND "+MATCH_COL+"=?",new String[]{tournament,Integer.toString(matchNum)});
+
+        cursor.moveToFirst();
+
+        return  cursor;
+
+    }
+
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
